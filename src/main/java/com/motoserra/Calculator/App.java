7f74af674688f87b00ab0d82f3267997fe68343a
THIAGO PIZZIO MEDINA <15104346@pucrs.br>
2018-08-21 10:59:17 -0300

    Primeiro commit

---
+++ src/main/java/com/motoserra/Calculator/App.java
@@ -0,0 +1,13 @@
+package com.motoserra.Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
