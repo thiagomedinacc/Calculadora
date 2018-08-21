6facecef6ef4b38521a25fe25edb61ca5c71f412
thiagomedinacc <thiago.medina@acad.pucrs.br>
2018-08-21 20:57:14 -0300

    Acrescenta metodo
    Metodo evaluateMenos permite realizar tanto somas quanto subtrações

--- src/main/java/com/motoserra/Calculator/Calculator.java
+++ src/main/java/com/motoserra/Calculator/Calculator.java
@@ -1,4 +1,5 @@
 package com.motoserra.Calculator;
+import java.util.ArrayList;
 
 public class Calculator {
 	
@@ -8,4 +9,26 @@ public class Calculator {
 	      sum += Integer.valueOf(summand);
 	    return sum;
 	  }
+	  
+	public int evaluateMenos(String expression) {
+    int sum = 0;
+    int qnt = expression.length();
+   
+    ArrayList<Character> p = new ArrayList();
+    for(int i = 0; i< qnt;  i++){
+        p.add(expression.charAt(i));
+    }
+    sum = Character.getNumericValue(p.get(0));
+    
+    for (int i = 1; i < qnt; i = i + 2){
+        if(p.get(i) == '+'){
+            sum += Character.getNumericValue(p.get(i+1));
+        }
+        else sum -= Character.getNumericValue(p.get(i+1));
+    }
+       
+    
+    return sum;
 	}
+	
+}
